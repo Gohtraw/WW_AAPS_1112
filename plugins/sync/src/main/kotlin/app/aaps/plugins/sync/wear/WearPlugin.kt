9a0a137979dcf79b806c0d29eb3761452536d854
Philoul <legoupil.jp@free.fr>
2024-11-15 18:17:07 +0100

    Wearplugin send UserActions to watch

--- plugins/sync/src/main/kotlin/app/aaps/plugins/sync/wear/WearPlugin.kt
+++ plugins/sync/src/main/kotlin/app/aaps/plugins/sync/wear/WearPlugin.kt
@@ -16,6 +16,7 @@ import app.aaps.core.interfaces.receivers.Intents
 import app.aaps.core.interfaces.resources.ResourceHelper
 import app.aaps.core.interfaces.rx.AapsSchedulers
 import app.aaps.core.interfaces.rx.bus.RxBus
+import app.aaps.core.interfaces.rx.events.EventAutomationUpdateGui
 import app.aaps.core.interfaces.rx.events.EventAutosensCalculationFinished
 import app.aaps.core.interfaces.rx.events.EventDismissBolusProgressIfRunning
 import app.aaps.core.interfaces.rx.events.EventLoopUpdateGui
@@ -111,6 +112,10 @@ class WearPlugin @Inject constructor(
             .toObservable(EventLoopUpdateGui::class.java)
             .observeOn(aapsSchedulers.io)
             .subscribe({ dataHandlerMobile.resendData("EventLoopUpdateGui") }, fabricPrivacy::logException)
+        disposable += rxBus
+            .toObservable(EventAutomationUpdateGui::class.java)
+            .observeOn(aapsSchedulers.io)
+            .subscribe({ dataHandlerMobile.sendUserActions() }, fabricPrivacy::logException)
         disposable += rxBus
             .toObservable(EventWearUpdateGui::class.java)
             .observeOn(aapsSchedulers.main)
